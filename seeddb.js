66d9c2990686f9bdad687d862ad14d3d0a869cec
Alejandro Balbi <alejandrob@bandofcoders.com>
2015-02-24 20:19:50 -0300

    Seeding nicer

--- seeddb.js
+++ seeddb.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/ronda');
 var User = require('./models/User');
+User.remove({}, function(err) { 
+   console.log('users collection removed') 
+	process.exit(1);
+});
+/*
 User.findOneAndRemove({username: "nicky"},function(err, item){
 	User.findOneAndRemove({username: "niccolo"},function(err, item){
-		process.exit(1);
 	})
 })
+*/
